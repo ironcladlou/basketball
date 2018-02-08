0887b508e1c6a0be296038ae08cdfa4e7c179ba8
Dan Mace <ironcladlou@gmail.com>
2018-02-07 20:29:30 -0500

    Kinematic path-based arm test

--- Court.gd
+++ Court.gd
@@ -1,21 +1,30 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	pass
+	$ArmPath/ArmPathFollow.offset = 0.1
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		$Ball.apply_impulse(Vector3(0, 0, 0), Vector3(0.5, 0.5, 0))
 	if Input.is_key_pressed(KEY_R):
 		reset()
+	if Input.is_key_pressed(KEY_W):
+		$ArmPath/ArmPathFollow.offset += 0.1
+	if Input.is_key_pressed(KEY_S):
+		$ArmPath/ArmPathFollow.offset -= 0.1
+
+	# Bound movement
+	if $ArmPath/ArmPathFollow.offset < 0.1:
+		$ArmPath/ArmPathFollow.offset = 0.1
+	if $ArmPath/ArmPathFollow.offset > 1:
+		$ArmPath/ArmPathFollow.offset = 1
+
+func _input(ev):
+	print("Input event:" + ev.as_text())
+	if ev is InputEventMouseMotion:
+		print("Mouse motion event: x=" + str(ev.relative.x) + ",y=" + str(ev.relative.y))
+	if ev is InputEventMouseButton:
+		print("Mouse button event: pressed", str(ev.pressed))
 
 func reset():
 	get_tree().reload_current_scene()
